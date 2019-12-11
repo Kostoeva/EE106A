3363c0f568c732af489e14c410be3fb6c11c421e
irmareka@gmail.com <ee106a-afy@c111-1.local>
2019-12-11 11:06:28 -0800

    adding subscriber to get current turtle bot vel and yaw

--- turtlebot_ar/devel/local_setup.sh
+++ turtlebot_ar/devel/local_setup.sh
@@ -3,7 +3,7 @@
 
 # since this file is sourced either use the provided _CATKIN_SETUP_DIR
 # or fall back to the destination set at configure time
-: ${_CATKIN_SETUP_DIR:=/home/cc/ee106a/fa19/class/ee106a-aha/Desktop/turtlebot_ar/devel}
+: ${_CATKIN_SETUP_DIR:=/scratch/turtlear/turtlebot_ar/devel}
 CATKIN_SETUP_UTIL_ARGS="--extend --local"
 . "$_CATKIN_SETUP_DIR/setup.sh"
 unset CATKIN_SETUP_UTIL_ARGS
--- turtlebot_ar/src/turtlebot_ar/src/navigation_waypoints.py
+++ turtlebot_ar/src/turtlebot_ar/src/navigation_waypoints.py
@@ -22,6 +22,7 @@ from geometry_msgs.msg import Twist
 
 
 class NavigationWaypoints():
+
     def __init__(self):
         # initiliaze
         rospy.init_node('Navigation_Waypoints', anonymous=False)
@@ -34,7 +35,7 @@ class NavigationWaypoints():
         
     # Create a publisher which can "talk" to TurtleBot and tell it to move
         # Tip: You may need to change cmd_vel_mux/input/navi to /cmd_vel if you're not using TurtleBot2
-        self.cmd_vel = rospy.Publisher('yellow/mobile_base/commands/velocity', Twist, queue_size=10)
+        self.cmd_vel = rospy.Publisher('red/mobile_base/commands/velocity', Twist, queue_size=10)
      
     #TurtleBot will stop if we don't keep telling it to move.  How often should we tell it to move? 10 HZ
         r = rospy.Rate(10);
@@ -82,6 +83,7 @@ class NavigationWaypoints():
         self.cmd_vel.publish(Twist())
     # sleep just makes sure TurtleBot receives the stop command prior to shutting down the script
         rospy.sleep(1)
+
  
 if __name__ == '__main__':
     try:
